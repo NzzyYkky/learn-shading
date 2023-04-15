d13f6146c990a4e03eedb302fed0227ba342f368
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2023-04-15 21:55:05 +0900

    add shadow

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -2,9 +2,12 @@ uniform vec3 uColor; // rgb
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
    vec4 textureColor = texture2D(uTexture, vUv);
    // gl_FragColor = vec4(uColor, 1.0);
+   // add shadow
+   textureColor.rgb *= vElevation * 2.4 + 0.7;
    gl_FragColor = textureColor;
 }
--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -2,16 +2,24 @@ uniform vec2 uFrequency;
 uniform float uTime;
 
 varying vec2 vUv;
+// add shadow
+varying float vElevation;
 
 void main() {
    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-   modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-   modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+   // add x move
+   float elevation = sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
+   // add y move
+   elevation += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+
+   modelPosition.z += elevation;
 
    vec4 viewPosition = viewMatrix * modelPosition;
    vec4 projectionPosition = projectionMatrix * viewPosition;
    gl_Position = projectionPosition;
    // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
    vUv = uv;
+
+   vElevation = elevation;
 } 
